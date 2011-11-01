69898e9cf2e71794721623c7c55078ccb13d571c
Matthias Bartelmeß <mba@fourplusone.de>
2011-11-01 16:35:51 +0100

    Added Support for Bitbucket

---
+++ Clone in GitX.safariextension/inject-bb.js
@@ -0,0 +1,16 @@
+var ending = ".git";
+
+$("#repo-desc-cloneinfo").each(function(i,o){
+
+	var cloneElement = $("a.ssh" ,o);
+	var cloneUrl = cloneElement.attr('href');
+	if(cloneUrl.substr(cloneUrl.length - ending.length, ending.length) == ending){
+		var gitXLink = 'gitx://clone/?repo=' + encodeURIComponent(cloneUrl);
+
+		var gitXLinkElement = $('<a>± GitX</a>');
+		gitXLinkElement.attr("href", gitXLink);
+		cloneElement.after($("<span> / </span>").append(gitXLinkElement));
+	}
+
+});
+
